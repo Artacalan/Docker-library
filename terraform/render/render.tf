dc527283c4fa1943692fc62e37dc7217957e16ca
Artacalan <christian94rz@gmail.com>
2025-04-03 09:50:19 +0200

    add terraform

---
+++ terraform/render/render.tf
@@ -0,0 +1,24 @@
+variable "render_api_key" {
+  type = string
+}
+
+variable "render_owner_id" {
+  type = string
+}
+
+terraform {
+  backend "local" {
+    path = "../backend.tfstate"  
+  }
+  required_providers {
+    render = {
+      source = "render-oss/render"
+      version = "1.6.0"
+    }
+  }
+}
+
+provider "render" {
+  api_key = var.render_api_key
+  owner_id = var.render_owner_id
+}
